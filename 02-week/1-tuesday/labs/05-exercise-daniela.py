c26e831c13e49a3ad5220aa20419039b129df467
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-03 14:46:22 -0500

    Failed at trying to solve

--- 02-week/1-tuesday/labs/05-exercise-daniela.py
+++ 02-week/1-tuesday/labs/05-exercise-daniela.py
@@ -2,3 +2,15 @@
 #(one returned from either letter_histogram or word_summary), 
 #print the top 3 words or letters.
 
+word = input("Enter word here:")
+
+word_count = {} 
+  
+for i in word: 
+    if i in word_count: 
+        word_count[i] += 1
+    else: 
+        word_count[i] = 1
+
+
+print(word_count)
