96bc7bd3734edbb8ce11edfb893085c50e168155
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-03 13:13:05 -0500

    Solving my excersie

--- 02-week/1-tuesday/labs/01-exercise-daniela.py
+++ 02-week/1-tuesday/labs/01-exercise-daniela.py
@@ -1,1 +1,16 @@
-print ('Hello World')
+#print ('Hello World')
+
+phonebook_dict = { 
+  'Alice': '703-493-1834', 
+  'Bob': '857-384-1234', 
+  'Elizabeth': '484-584-2923'
+}
+
+#print(phonebook_dict['Elizabeth'])
+phonebook_dict['Kareem'] = '938-489-1234'
+phonebook_dict.pop('Alice')
+
+phonebook_dict['Bob'] = '986-345-2345'
+#print(phonebook_dict['Bob'])
+
+print(phonebook_dict)
