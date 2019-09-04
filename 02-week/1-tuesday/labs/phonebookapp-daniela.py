e32db5af895c20c0d6f4fae32337f21768f71ba4
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-04 00:00:10 -0500

    Trying to resolve

--- 02-week/1-tuesday/labs/phonebookapp-daniela.py
+++ 02-week/1-tuesday/labs/phonebookapp-daniela.py
@@ -10,35 +10,29 @@ phonebook = {
 #3. Delete an entry 
 #4. List all entries 
 #5. Quit
-
+  
 
 # This searches for a user
-search_user = input("Enter name here:")
-print(phonebook[search_user])
+# search_user = input("Enter name here:")
+# print(phonebook[search_user])
 
 #This adds new user with number
 
-new_user_name = input("New concact name")
-new_user_number = input("New number for user")
-phonebook[new_user_name] = new_user_number
-print(phonebook)
+# new_user_name = input("New concact name")
+# new_user_number = input("New number for user")
+# phonebook[new_user_name] = new_user_number
+# print(phonebook)
 
 #This deletes a user
-delete_user = input("Name of user you want to delete")
-phonebook.pop(delete_user)
-print(phonebook)
+# delete_user = input("Name of user you want to delete")
+# phonebook.pop(delete_user)
+# print(phonebook)
 
 #To print all enteries 
 
-print_all = ("Do you wish to print all contacts? Yes/No")
+# print_all = ("Do you wish to print all contacts? Yes/No")
 
-if print_all == 'Yes':
-    print(phonebook)
-else:
-    print("OK")
-
-
-
-
-
-
+# if print_all == 'Yes':
+#    print(phonebook)
+#else:
+#    print("OK")
