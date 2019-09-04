700cae33dbf156335435743b07a2d81459ce1b7a
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-04 13:59:45 -0500

    Finished project

--- 02-week/2-wednesday/labs/01-madlib-daniela.py
+++ 02-week/2-wednesday/labs/01-madlib-daniela.py
@@ -1,2 +1,12 @@
-print ("Hello world !")
+
+def function(a, b):
+    print(f'{a} and {b} sitting in a tree, K-I-S-S-I-N-G!')
+    print('First came love, then came marriage')
+    print('then comes baby in a baby carriage!')
+
+a = input('give me name ')
+b = input('give me another name ')
+
+function(a, b)
+
 
