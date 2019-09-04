59ca2c15d37678555a6f1b0a080a2c44ccca73ba
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-04 15:34:26 -0500

    Updated code

--- 02-week/1-tuesday/labs/phonebookapp-daniela.py
+++ 02-week/1-tuesday/labs/phonebookapp-daniela.py
@@ -1,38 +1,54 @@
+def print_menu():
+    print('========================')
+    print('1. Look up an entry')
+    print('2. Set an entry') 
+    print('3. Delete an entry') 
+    print('4. List all entries') 
+    print('5. Quit') 
+    
 phonebook = {
     'Jon': '123-123-1234',
     'Sansa': '321-321-4321',
     'Ayra': '456-456-4567',
 }
+menu_choice = 0
+print_menu()
 
-#print("Electronic Phone Book")
-#1. Look up an entry 
-#2. Set an entry 
-#3. Delete an entry 
-#4. List all entries 
-#5. Quit
-  
-
+while menu_choice != 5:
+    menu_choice = int(input("What do you want to do (1-5)?"))
+    if menu_choice == 1:
+        print("Look up an entry")
+        search_user = input("Name:")
+        if search_user in phonebook:
+            print(phonebook[search_user])
 # This searches for a user
 # search_user = input("Enter name here:")
 # print(phonebook[search_user])
+    elif menu_choice == 2:
+        print("Add Name and Number")
+        new_user = input("Name: ")
+        new_user_number = input("Number: ")
+        phonebook[new_user] = new_user_number
 
 #This adds new user with number
-
-# new_user_name = input("New concact name")
+# new_user = input("New concact name")
 # new_user_number = input("New number for user")
-# phonebook[new_user_name] = new_user_number
-# print(phonebook)
-
+# phonebook[new_user] = new_user_number
+    elif menu_choice == 3:
+        print("Delete an entry")
+        delete_user = input("Name: ")
+        if delete_user in phonebook:
+            phonebook.pop(delete_user)
 #This deletes a user
 # delete_user = input("Name of user you want to delete")
 # phonebook.pop(delete_user)
 # print(phonebook)
-
+    elif menu_choice == 4:
+        print("List all entries")
+        for x in phonebook.keys():
+            print("Name: ", x, "Number", phonebook[x])
+        print()
+    elif menu_choice != 5:
+        print_menu    
 #To print all enteries 
-
-# print_all = ("Do you wish to print all contacts? Yes/No")
-
-# if print_all == 'Yes':
-#    print(phonebook)
-#else:
-#    print("OK")
+         
