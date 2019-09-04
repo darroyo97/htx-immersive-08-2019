8e17382a351f75445d30ba18fe7647e3f6fc27d4
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-04 14:49:34 -0500

    project updated

--- 02-week/2-wednesday/labs/04-evennum-daniela.py
+++ 02-week/2-wednesday/labs/04-evennum-daniela.py
@@ -1,10 +1,11 @@
 def isEven(x):
-    if x % 2 ==0:
+    if x % 2 == 0:
         print("True")
+        return True
     else:
         print("False")
+        return False
 
 x = int(input("Number?"))
 
 isEven(x)
-
