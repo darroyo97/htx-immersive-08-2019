075715818246ab2d7d4f9b587225c93b855868c8
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-05 14:48:11 -0500

    New file for Phonebook project

--- 02-week/3-thursday/labs/phonebook/add.py
+++ 02-week/3-thursday/labs/phonebook/add.py
@@ -1,1 +1,17 @@
 #this will hold my add functions
+import contacts
+import view
+from lookup import lookup
+
+def add():
+    last_name = input('Last Name:')
+    first_name = input('First Name:')
+    phone_number = input('Phone Number:')
+    contacts.contacts.append({
+        'first_name': first_name,
+        'last_name': last_name,
+        'phone': phone_number
+    })
+    print("New contact successfully added.")
+
+    
