befaee0e6ba4ebeced74989f3880d22d072d8355
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-05 09:16:53 -0500

    updating code to shorter version

--- 02-week/2-wednesday/labs/05-oddnum-daniela.py
+++ 02-week/2-wednesday/labs/05-oddnum-daniela.py
@@ -1,11 +1,12 @@
 import evennum_daniela
 
-def isOdd(x):
-    if evennum_daniela.isEven(x) == True:
-        return False
-    else:
-        return True
+#def isOdd(x):
+#    if evennum_daniela.isEven(x) == True:
+#        return False
+#    else:
+#        return True
 
-x = int(input("Number?"))
+#x = int(input("Number?"))
 
-print(isOdd(x))
+#print(isOdd(x))
+
--- 02-week/2-wednesday/labs/evennum_daniela.py
+++ 02-week/2-wednesday/labs/evennum_daniela.py
@@ -1,12 +1,20 @@
-def isEven(x):
-    if x % 2 == 0:
+#def isEven(x):
+#    if x % 2 == 0:
         #print("True")
-        return True
-    else:
-        #print("False")
-        return False
+#        return True
+#    else:
+#        #print("False")
+#        return False
     
 
 #x = int(input("Number?"))
 
 #print(isEven(x))
+
+
+#same function easier 
+
+#def isEven(x):
+#    return (x % 2 == 0)
+
+#print(isEven(6))
