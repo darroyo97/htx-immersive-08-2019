532230c164408a1f14568d3a4527b738a000bcaa
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-05 11:23:36 -0500

    updated code

--- 02-week/2-wednesday/labs/06-onlyevens-daniela.py
+++ 02-week/2-wednesday/labs/06-onlyevens-daniela.py
@@ -1,8 +1,15 @@
-numbers = [11, 20, 42, 97, 23, 10]
+#numbers = [11, 20, 42, 97, 23, 10]
 
-def isEven():
-    for num in numbers:
-        if num % 2 == 0:
-            print(num)
+#def isEven():
+#    for num in numbers:
+#        if num % 2 == 0:
+#            print(num)
 
-isEven()
+#isEven()
+
+#def only_evens(list_of_numbers):
+#    result = []
+#    for number in list_of_numbers:
+
+
+#only_evens =([11, 20, 42, 97, 23, 10])
