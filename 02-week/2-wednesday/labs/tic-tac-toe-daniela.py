407e2c47671d01b548e9f854175d042655ec4235
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-05 15:59:50 -0500

    wrote code for board

--- 02-week/2-wednesday/labs/tic-tac-toe-daniela.py
+++ 02-week/2-wednesday/labs/tic-tac-toe-daniela.py
@@ -1,7 +1,8 @@
-board = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-]
+board = [[0,1,2], [3,4,5], [6,7,8]]
 
-#def move(board, location, player):
+def draw_board(board):
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+draw_board(board)
