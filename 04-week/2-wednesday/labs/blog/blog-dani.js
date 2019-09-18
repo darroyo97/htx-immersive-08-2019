69c740bfc55e5ee2cf0297c3c7cf5fcbbe21d02d
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-18 14:53:19 -0500

    added title in header

--- 04-week/2-wednesday/labs/blog/blog-dani.js
+++ 04-week/2-wednesday/labs/blog/blog-dani.js
@@ -1,1 +1,18 @@
-document.getElementById("container").innerHTML = "Help!";
+buildPage()
+
+function buildPage() {
+    var container = document.getElementById("container");
+    container.appendChild(buildHeader());
+}
+
+function buildHeader() {
+    var header = document.createElement("header");
+    header.appendChild(buildTitle());
+    return header;
+}
+
+function buildTitle() {
+    var title = document.createElement("h1");
+    title.textContent = "High on Coding";
+    return title;
+}
