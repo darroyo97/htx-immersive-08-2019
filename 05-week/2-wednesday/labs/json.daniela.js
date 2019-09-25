8a1472b2cc07039d1d463d0d9efd16fc3d8f5b60
Daniela Arroyo <daniela.arroyo27@gmail.com>
2019-09-25 12:06:13 -0500

    adding link

--- 05-week/2-wednesday/labs/json.daniela.js
+++ 05-week/2-wednesday/labs/json.daniela.js
@@ -1,40 +1,41 @@
-//initialize http request
+// This is the client-side script.
+
+// Initialize the HTTP request.
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://www.omdbapi.com/?t=shrek&y=2001");
+xhr.open("GET", "http://www.omdbapi.com/?i=tt0126029&apikey=e03ca193");
 
-//track the state
-
+// Track the state changes of the request.
 xhr.onreadystatechange = function() {
-    var DONE = 4; // readyState 4 means the request is done.
-    var OK = 200; // status 200 is a successful return.
-    if (xhr.readyState === DONE) {
-      if (xhr.status === OK) {
-        console.log(xhr.responseText); // 'This is the output.'
-        console.log(JSON.parse(xhr.responseText));
-      } else {
-        console.log("Error: " + xhr.status); // An error occurred during the request.
-      }
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+      console.log(xhr.responseText); // 'This is the output.'
+      console.log(JSON.parse(xhr.responseText));
+    } else {
+      console.log("Error: " + xhr.status); // An error occurred during the request.
     }
-  };
-  xhr.send(null);
-
-  // AJAX
-$.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/?t=shrek&y=2001",
-    dataType: "JSON", // data type expected from server
-    success: function(response) {
-      console.log(response);
-      showMovieDetails(response.Title, response.Released);
-    },
-    error: function(error) {
-      console.log("Error: " + error);
-    }
-  });
-
-  function showMovieDetails(title, released) {
-    var heading = $("<h1/>").text(title);
-    $("body").append(heading);
-    var released = $("<h2/>").text(released);
-    $("body").append(released);
   }
+};
+xhr.send(null);
+
+// AJAX
+$.ajax({
+  type: "GET",
+  url: "http://www.omdbapi.com/?i=tt0126029&apikey=e03ca193",
+  dataType: "JSON", // data type expected from server
+  success: function(response) {
+    console.log(response);
+    showMovieDetails(response.Title, response.Released);
+  },
+  error: function(error) {
+    console.log("Error: " + error);
+  }
+});
+
+function showMovieDetails(title, released) {
+  var heading = $("<h1/>").text(title);
+  $("body").append(heading);
+  var released = $("<h2/>").text(released);
+  $("body").append(released);
+}
